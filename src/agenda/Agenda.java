0cc62601ff98dbed6e318e53aa24daa0bac6c729
findag <findag@findag-TOSH>
2014-06-12 10:59:29 +0200

    Primera parte, Ventanas

---
+++ src/agenda/Agenda.java
@@ -0,0 +1,22 @@
+/*
+ * Vamos a desarrollar un proyecto para una agenda, la persona tendra primero que darse de alta y despues
+ * podra insertar sus contactos.
+ */
+
+package agenda;
+
+/**
+ *
+ * @author findag
+ */
+public class Agenda {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args)
+    {
+        
+    }
+    
+}
